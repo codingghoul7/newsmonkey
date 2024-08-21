f40cb9bac4dc2d277dc7cb11c206740830c1f2e6
CodingGhoul <108980953+codingghoul7@users.noreply.github.com>
2024-08-21 19:15:55 +0530

    pre&forward button added

--- src/component/Newsitem.jsx
+++ src/component/Newsitem.jsx
@@ -2,21 +2,26 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+
 export class Newsitem extends Component {
+
+
+
+
   render() {
 
-    let {card_title,card_des}=this.props
+    let {card_title,card_des,imgurl,newsurl}=this.props
 
     return (
     
       <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src="holder.js/100px180" />
+      <Card.Img variant="top" src={imgurl ? imgurl : " "} />
       <Card.Body>
         <Card.Title>{card_title}</Card.Title>
         <Card.Text>
        {card_des}
         </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
+        <Button variant="primary" href={newsurl} target="_blank" >Go somewhere</Button>
       </Card.Body>
     </Card>
 
