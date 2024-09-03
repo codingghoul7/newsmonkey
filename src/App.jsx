8c4f23627a19a6a0bec1ff6d83f51e5324b8f3f5
CodingGhoul <108980953+codingghoul7@users.noreply.github.com>
2024-09-03 20:33:11 +0530

    code refactor
    refactored all the code using ai

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./component/Navi.css";
-import Nabar from "./component/Navi";
-import News from "./component/News";
-import "./App.css";
+import { useState } from 'react'
+import card from './components/card'
+import './App.css'
 
-export default class App extends Component {
-  render() {
-    return (
-      <>
-        <Nabar />
-        <News pageSize={6} />
-       
-      </>
-    );
-  }
+function App() {
+ 
+
+  return (
+    <>
+      <card/>
+    </>
+  )
 }
+
+export default App
